a0bbecc9ed11361031bd958d104abbf1c01c9520
Justin Napolitano <napolitano.just@gmail.com>
2025-11-23 04:10:51 -0500

    chore: auto-generate blog index

--- archetypes/default.md
+++
@@ -1,7 +0,0 @@
-+++
-title = '{{ replace .File.ContentBaseName "-" " " | title }}'
-date = {{.lastmod}}
-draft = true
-+++
-
-<!-- date = {{ .Date }} -->
--- public/posts/flask-api-updater/src/create-repo.sh
+++
@@ -1,9 +0,0 @@
-
-# Create the Artifact Registry repository
-gcloud artifacts repositories create rss-updater \
-    --repository-format=docker \
-    --location=$REGION \
-    --description="Docker repository for RSS Updater"
-
-# Verify the repository creation
-gcloud artifacts repositories list --location=$REGION
